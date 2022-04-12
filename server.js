4d544254f258762a444a0cf4f19769db8883723d
mohammed <imyacoob86@gmail.com>
2022-04-12 08:07:44 +0000

    pushing new changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('TCS DEMO - Container Hardening and Zero touch deployment - completed');
+  res.send('TCS DEMO - Container Hardening and Zero touch deployment - docker_web_app: completed');
 });
 
 app.listen(PORT, HOST);
